cbb3d784fcb603e40e0efeddb6cc6e54ea2ed34e
Davis Wertheimer <dww78@cornell.edu>
2025-08-27 17:18:28 -0400

    Add wacky 1b for triton testing

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -336,6 +336,19 @@ def get_model_config(model_variant):
             rope_scaling={"rope_type":"unrope"},
             rope_partial=.25,
         )
+    elif model_variant == "llama_1b_triton":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1024,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.5,
+            max_expected_seq_len=4096,
+            rope_theta=32,
+            rope_scaling={"rope_type":"unrope"},
+            rope_partial=.25,
+        )
     elif model_variant == "mamba_9.8b_32k":
         model_config = {
             "d_model": 4096,
