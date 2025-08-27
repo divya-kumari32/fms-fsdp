d7d53d1f21ac934033302100fe5a977812fa5e07
Davis Wertheimer <dww78@cornell.edu>
2025-08-27 10:16:32 -0400

    Pass trivial dp degree

--- main_training_llama.py
+++ main_training_llama.py
@@ -75,7 +75,7 @@ def main(**kwargs):
     if rank == 0:
         print("Constructing datasets...")
     if not cfg.use_dummy_dataset:
-        train_loader = get_data_loader(cfg, rank, world_size)
+        train_loader = get_data_loader(cfg, rank, world_size, world_size)
     else:
         train_loader = get_dummy_loader(cfg, rank, world_size)
     if rank == 0:
