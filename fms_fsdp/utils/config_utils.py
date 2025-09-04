7b49985a9dea6d7174c5e25cea2761921e6e186f
Davis Wertheimer <dww78@cornell.edu>
2025-09-04 17:26:17 -0400

    Triton head dim pow2

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -339,11 +339,11 @@ def get_model_config(model_variant):
     elif model_variant == "llama_1b_triton":
         model_config = LLaMAConfig(
             src_vocab_size=128256,
-            emb_dim=1024,
-            nheads=16,
-            kvheads=4,
+            emb_dim=1280,
+            nheads=20,
+            kvheads=5,
             nlayers=32,
-            hidden_grow_factor=3.5,
+            hidden_grow_factor=3.2,
             max_expected_seq_len=4096,
             rope_theta=32,
             rope_scaling={"rope_type":"unrope"},
