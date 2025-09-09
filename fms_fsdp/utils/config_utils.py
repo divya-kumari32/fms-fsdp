e92b9efc8d9e93c3ef03456e6a1c4377c11ba158
Davis Wertheimer <dww78@cornell.edu>
2025-09-09 19:33:50 -0400

    Add halfrope cfg

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -299,6 +299,18 @@ def get_model_config(model_variant):
             max_expected_seq_len=4096,
             rope_theta=500000.0,
         )
+    elif model_variant == "llama_1b_halfrope":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=4096,
+            rope_theta=500000.0,
+            rope_partial=.5,
+        )
     elif model_variant == "llama_1b_rope652":
         model_config = LLaMAConfig(
             src_vocab_size=128256,
