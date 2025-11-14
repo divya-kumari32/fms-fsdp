a268c297876a919dd79fd6d0cd3883682b5bf4b0
Davis Wertheimer <dww78@cornell.edu>
2025-11-13 22:02:16 -0500

    Add longseq 1b cfgs

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -348,6 +348,54 @@ def get_model_config(model_variant):
             rope_scaling={"rope_type":"unrope"},
             rope_partial=.5,
         )
+    elif model_variant == "llama_1b_unrope32_32k":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=32768,
+            rope_theta=32,
+            rope_scaling={"rope_type":"unrope"},
+            rope_partial=.5,
+        )
+    elif model_variant == "llama_1b_unrope32_128k":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=131072,
+            rope_theta=32,
+            rope_scaling={"rope_type":"unrope"},
+            rope_partial=.5,
+        )
+    elif model_variant == "llama_1b_32k":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=32768,
+            rope_theta=500000.0,
+        )
+    elif model_variant == "llama_1b_128k":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=131072,
+            rope_theta=500000.0,
+        )
     elif model_variant == "llama_1b_unrope32_triton":
         model_config = LLaMAConfig(
             src_vocab_size=128256,
