5c4a3d9f92bc3cd65be16e3e33ce792bad935e7c
Davis Wertheimer <dww78@cornell.edu>
2026-03-17 23:14:42 -0400

    Add pure mamba 1b

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -232,6 +232,31 @@ def get_model_config(model_variant):
             "pad_vocab_size_multiple": 16,
             "tie_embeddings": False,
         }
+    elif model_variant == "mamba_1b_pure_long":
+        model_config = {
+            "d_model": 1280,
+            "d_intermediate": 3072,
+            "n_layer": 32,
+            "vocab_size": 128256,
+            "ssm_cfg": {"layer": "Mamba2"},
+            "attn_layer_idx": [],
+            "attn_cfg": {
+                "causal": True,
+                "d_conv": 0,
+                "head_dim": 128,
+                "num_heads": 10,
+                "num_heads_kv": 5,
+                "out_proj_bias": False,
+                "qkv_proj_bias": False,
+                "rotary_emb_dim": 0,
+                "rotary_emb_base": 10_000,
+            },
+            "rms_norm": True,
+            "residual_in_fp32": True,
+            "fused_add_norm": True,
+            "pad_vocab_size_multiple": 16,
+            "tie_embeddings": False,
+        }
     elif model_variant == "mamba_1b_rope":
         model_config = {
             "d_model": 1280,
